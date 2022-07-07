fdb6e50051eeea63fe576e828241107be5a1a983
Cesar Muñoz <cesar.alberto.munoz@gmail.com>
2022-07-07 19:59:48 +0200

    fix: get base url for vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,15 +7,34 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
+const getBaseUrl = () => {
+  if (process.browser) return "";
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  }
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+};
+
 export default withTRPC<AppRouter>({
   config({ ctx }) {
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : `http://localhost:3000/api/trpc`;
+    const url = `${getBaseUrl()}/api/trpc`;
 
     return {
       url,
     };
   },
   ssr: true,
+  responseMeta({ clientErrors, ctx }) {
+    if (clientErrors.length) {
+      // propagate first http error from API calls
+      return {
+        status: clientErrors[0].data?.httpStatus ?? 500,
+      };
+    }
+    // cache full page for 1 day + revalidate once every second
+    const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+    return {
+      "Cache-Control": `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+    };
+  },
 })(MyApp);
